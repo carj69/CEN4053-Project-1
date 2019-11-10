6b802fb67c2dce1f2ab389ee59752f42e1a19c26
carj69 <43427326+carj69@users.noreply.github.com>
2019-11-10 16:10:43 -0600

    Update hotburger.sh

--- hotburger.sh
+++ hotburger.sh
@@ -1,11 +1,11 @@
 #Reference https://docs.docker.com/compose/
 
 # Create a volume
-docker volume create logVolume
+# docker volume create logVolume
 
 
 # build and start the containers using yaml file.
-docker-compose build -d
+docker-compose build
 docker-compose up
 
 # To stop use
